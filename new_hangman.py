323244d2bc177a64c787e06aedaad7533591460d
Estebyte <estebanloaizamora@gmail.com>
2024-08-20 15:10:40 +0100

    Added better comments and change the try of the input filter

--- new_hangman.py
+++ new_hangman.py
@@ -2,37 +2,47 @@ import random
 import os
 
 def select_word(path):
+    # Create a list with the words of 'data.txt' and pick a random word.
     with open(path, "r", encoding="utf-8") as file:
         data = [i[:-1]for i in file]
     word = random.choice(data)
     return word
 
 def run():
+    # Create the word the user will try to find.
     word = (select_word("Python/Pruebas/data.txt"))
+
+    # Set the numbers of attemps with a simple operation with lenght of the word
     attemps = len(word) + (len(word) // 2)
+
+    # Create an underlines list with the same lenght than the word
     underlines = list("_" * len(word))  
 
     while True:
         os.system("cls")
+        # A dirty game cheat! uncomment to activate.
         #print(word)
         print(underlines)
         letter = input(f"Hangman! Guess the word, you have {attemps} attemps ==>").lower()
 
+        # Input filter. It will only allow to enter one alphabet letter
         try:
-            if letter.isalpha() == False or len(letter) != 1:
+            if not letter.isalpha() or len(letter) != 1:
                 raise Exception("Oops, you can only enter one letter")
         except Exception as error:
             os.system("cls")
             print(error)
             continue
             
-        #Search letter in word    
+        # Search letter in word.   
         if letter in word:
-            for index, values in enumerate(word): #Change "_" in underlines by letter
+            # Change "_" in underlines by letter when the letter is in  the word.
+            for index, values in enumerate(word): 
                 if values == letter:
                     underlines[index] = letter
             os.system("cls")
         else:
+            # When it's not, take one attemp.
             attemps -=1
             os.system("cls")
 
